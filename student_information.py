2c0e84fe2b0d43376e761bbcdce0df2138880157
Bahar ghasem of <bahar2009gasemof@gmail.com>
2026-07-23 23:50:04 +0330

    Version 1.3: Added input validation

--- student_information.py
+++ student_information.py
@@ -1,39 +1,51 @@
 system_name = "Student Information System"
 
+
 def student_information(name,age=17,**kwargs):
     print(f"=== Welcome to the {system_name} ===")
     print("\n===== Student Information =====")
     print(f"Name        : {name}")
     print(f"Age         : {age}")
-    
+
     for key, value in kwargs.items():
         print(f"{key.capitalize():12}: {value}")
 
-name = input("Enter your name: ")
-age = input("Enter your age (Press Enter for 17): ")
-country = input("Enter your country:")
-city = input("Enter your city: ")
-major = input("Enter your major: ")
-institution= input("Enter your school or university: ")
 
-try:   
-    if age == "":
-        student_information(
-            name,
-            country=country,
-            city=city,
-            major=major,
-            institution=institution
-        )
-    else:
-        student_information(
-            name,
-            age=int(age),
-            country=country,
-            city=city,
-            major=major,
-            institution=institution
-        )
-        
-except:
-    print("Invalid age! Please enter a number.")
+name = input("Enter your name: ")
+
+if name == "":
+    print("Name cannot be empty!")
+
+else:
+    age = input("Enter your age (Press Enter for 17): ")
+
+    try:
+        if age == "":
+            age = 17
+        else:
+            age = int(age)
+
+        if age < 0:
+            print("Age cannot be negative!")
+
+        else:
+            country = input("Enter your country: ")
+            city = input("Enter your city: ")
+            major = input("Enter your major: ")
+            institution = input("Enter your school or university: ")
+
+            if country == "":
+                print("Country cannot be empty!")
+
+            else:
+                student_information(
+                    name,
+                    age,
+                    country=country,
+                    city=city,
+                    major=major,
+                    institution=institution
+                )
+
+    except:
+        print("Invalid age! Please enter a number.")
